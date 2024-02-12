988b49f66360925de7e30b74de5ae9b0387aa282
lucasaugsue <lucasaugsue7@gmail.com>
2024-02-12 20:36:17 -0300

    preparando para o vercel

---
+++ api/app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+from carros import carros_bp
+
+app = Flask(__name__)
+app.config['JSON_SORT_KEYS'] = False
+
+app.register_blueprint(carros_bp, url_prefix='/')
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ api/bd.py
@@ -0,0 +1,22 @@
+Carros = [
+    {
+        'id': 1,
+        'name': "McLaren",
+        'year': 2018
+    },
+    {
+        'id': 2,
+        'name': "Ferrari",
+        'year': 2019
+    },
+    {
+        'id': 3,
+        'name': "Lamborghini",
+        'year': 2020
+    },
+    {
+        'id': 4,
+        'name': "Porsche",
+        'year': 2021
+    }
+]
