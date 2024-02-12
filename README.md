06d9b9179d20dc13bfd0028a3e39dc9f3feaa012
lucasaugsue <lucasaugsue7@gmail.com>
2024-02-12 20:15:38 -0300

    add: fazendo o crud de carros

---
+++ README.md
@@ -0,0 +1,29 @@
+## Available Scripts (Scripts Disponíveis)
+
+No diretório do projeto, você pode executar:
+
+Crie um ambiente virtual usando o seguinte comando:
+### `python -m venv .venv`
+
+
+Ative o ambiente virtual. O método de ativação depende do seu sistema operacional:
+No Windows:
+### `.venv\Scripts\activate`
+
+No Linux/Mac:
+### `source .venv/bin/activate`
+
+
+Instalar Dependências:
+### `pip install -r requirements.txt`
+
+
+Rodar o projeto:
+### `flask run --reload`
+
+Executa o aplicativo no modo de desenvolvimento.\
+Abra [http://localhost:5000](http://localhost:5000) para visualizá-lo em seu navegador.
+
+
+## Documentation (Documentação)
+- [Flask](https://flask.palletsprojects.com/en/3.0.x/)
--- app.py
+++ app.py
@@ -1,29 +1,10 @@
-from flask import Flask, make_response, jsonify, request
-from bd import Carros
+from flask import Flask
+from carros import carros_bp
 
 app = Flask(__name__)
 app.config['JSON_SORT_KEYS'] = False
 
-@app.route("/carros", methods=["GET"])
-def get_carros():
-    return make_response(
-        jsonify(
-            message="Carros recuperados com sucesso!",
-            data=Carros
-        )
-    )
-
-@app.route('/carros', methods=['POST'])
-def create_carro():
-    carro = request.get_json()
-    Carros.append(carro)
-
-    return make_response(
-        jsonify(
-            message="Carro criado com sucesso!",
-            data=carro
-        )
-    )
+app.register_blueprint(carros_bp, url_prefix='/')
 
 @app.route("/")
 def hello_world():
